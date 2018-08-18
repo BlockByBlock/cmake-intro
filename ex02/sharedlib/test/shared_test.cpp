3d69e75c9a5ba7ccfc83c24c3a01d39209cdcfb8
blockbyblock <ybingcheng@gmail.com>
2018-08-18 11:47:32 +0800

    Revised examples

--- ex02/sharedlib/test/shared_test.cpp
+++
@@ -1,9 +0,0 @@
-#include "shared.h"
-
-int main(int argc, char** argv)
-{
-    SharedLibrary lobj;
-    lobj.printTest();
-
-    return 0;
-}
--- ex02/staticlib/test/static_test.cpp
+++
@@ -1,9 +0,0 @@
-#include "static.h"
-
-int main(int argc, char** argv)
-{
-    StaticLibrary tobj;
-    tobj.printTest();
-
-    return 0;
-}
